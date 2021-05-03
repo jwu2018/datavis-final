f1bc90346595f26dc04c790ac053a2287ede6b27
jwu2018 <jyalu.wu@hotmail.com>
2021-05-03 15:00:59 -0400

    bar chart and hop use imo's gen data

--- bar-charts.js
+++ bar-charts.js
@@ -6,7 +6,119 @@
  * @description    :  Bar charts using d3.js
  *------------------------------------------------------------------------------------------**/
 
-const num_data_points = 12
+/***************************************************************** */
+/* data generation
+/***************************************************************** */
+
+function generate_hourly_data(pop_range_start = 0, pop_range_end = 1, type = null, n = 24) {
+    // start with a totally random pop between pop_range_start and pop_range_end
+    let pops = []
+    pops[0] = Math.round(random_in_range(pop_range_start, pop_range_end) * 20) / 20
+
+    // then decide randomly ascending or descending pop
+    let isAscending = !!Math.round(Math.random())
+
+    // 90% chance to continue ascending or descending based on that decision
+    // switch to descending once hit 90%+
+    // change pop by increments of 0-20%
+
+    let decision_boundary = .8
+    let max_increment = (pop_range_end - pop_range_start) / 4
+
+    let decision = Math.random()
+
+    for (let i = 1; i < n; i++) {
+        decision = Math.random()
+        increment = random_in_range(0, max_increment)
+
+        if ((decision >= decision_boundary && isAscending) || (decision < decision_boundary && !isAscending)) {
+            // ascending
+            pops[i] = pops[i - 1] + increment
+        } else {
+            // descending
+            pops[i] = pops[i - 1] - increment
+        }
+
+        if (pops[i] >= pop_range_end) { // reached max pop
+            isAscending = !isAscending
+            if (pops[i] > pop_range_end) {
+                pops[i] = pop_range_end
+            }
+        }
+        if (pops[i] <= pop_range_start) { // reached min pop
+            isAscending = !isAscending
+            if (pops[i] < pop_range_start) {
+                pops[i] = pop_range_start
+            }
+        }
+        pops[i] = Math.round(pops[i] * 20) / 20
+    }
+
+    // inches of precipitation
+    let amounts = []
+    let min_amount = 0
+    let max_amount = .6
+    let no_rain_boundary = .1
+
+    for (let i = 0; i < pops.length; i++) {
+        // if (pops[i] <= no_rain_boundary) {
+        //     amounts[i] = min_amount
+        // } else {
+        amounts[i] = random_in_range(min_amount, max_amount) * pops[i]
+        if (pops[i] >= .1) {
+            amounts[i] += .05
+        }
+        amounts[i] = Math.round(amounts[i] * 20) / 20
+        // }
+    }
+    return [amounts, pops]
+    // return pops
+}
+
+function random_in_range(range_start = 0, range_end = 1) {
+    return Math.random() * (range_end - range_start) + range_start
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**------------------------------------------------------------------------
+ *                           BAR CHARTS COMMENCE
+ *------------------------------------------------------------------------**/
+const num_data_points = 24
 
 const margin = { 
     top: window.innerHeight / 10, 
@@ -95,6 +207,7 @@ function gen_rain_data(ndatapoints) {
     return d
 }
 
-build_barchart(gen_rain_data(num_data_points))
+// build_barchart(gen_rain_data(num_data_points))
+build_barchart(generate_hourly_data())
 
 // export{fillChart, gen_data, CHARTS, indices_to_compare, DATAPOINT_COUNTS}
--- d3-hops.js
+++ d3-hops.js
@@ -6,7 +6,119 @@
  * @description    :  Bar charts using d3.js
  *------------------------------------------------------------------------------------------**/
 
-const num_data_points = 3
+
+
+/***************************************************************** */
+/* data generation
+/***************************************************************** */
+
+function generate_hourly_data(pop_range_start = 0, pop_range_end = 1, type = null, n = 24) {
+    // start with a totally random pop between pop_range_start and pop_range_end
+    let pops = []
+    pops[0] = Math.round(random_in_range(pop_range_start, pop_range_end) * 20) / 20
+
+    // then decide randomly ascending or descending pop
+    let isAscending = !!Math.round(Math.random())
+
+    // 90% chance to continue ascending or descending based on that decision
+    // switch to descending once hit 90%+
+    // change pop by increments of 0-20%
+
+    let decision_boundary = .8
+    let max_increment = (pop_range_end - pop_range_start) / 4
+
+    let decision = Math.random()
+
+    for (let i = 1; i < n; i++) {
+        decision = Math.random()
+        increment = random_in_range(0, max_increment)
+
+        if ((decision >= decision_boundary && isAscending) || (decision < decision_boundary && !isAscending)) {
+            // ascending
+            pops[i] = pops[i - 1] + increment
+        } else {
+            // descending
+            pops[i] = pops[i - 1] - increment
+        }
+
+        if (pops[i] >= pop_range_end) { // reached max pop
+            isAscending = !isAscending
+            if (pops[i] > pop_range_end) {
+                pops[i] = pop_range_end
+            }
+        }
+        if (pops[i] <= pop_range_start) { // reached min pop
+            isAscending = !isAscending
+            if (pops[i] < pop_range_start) {
+                pops[i] = pop_range_start
+            }
+        }
+        pops[i] = Math.round(pops[i] * 20) / 20
+    }
+
+    // inches of precipitation
+    let amounts = []
+    let min_amount = 0
+    let max_amount = .6
+    let no_rain_boundary = .1
+
+    for (let i = 0; i < pops.length; i++) {
+        // if (pops[i] <= no_rain_boundary) {
+        //     amounts[i] = min_amount
+        // } else {
+        amounts[i] = random_in_range(min_amount, max_amount) * pops[i]
+        if (pops[i] >= .1) {
+            amounts[i] += .05
+        }
+        amounts[i] = Math.round(amounts[i] * 20) / 20
+        // }
+    }
+    return [amounts, pops]
+    // return pops
+}
+
+function random_in_range(range_start = 0, range_end = 1) {
+    return Math.random() * (range_end - range_start) + range_start
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**------------------------------------------------------------------------
+ *                           BUILDING HOPS
+ *------------------------------------------------------------------------**/
+const num_data_points = 24
 const std_dev = 0.02 // std dev for the normal distribution
 
 let svg, og_predictions, og_chances
@@ -38,12 +150,10 @@ function remove_svg() {
 }
 
 function build_svg() {
-    // append the svg object to the div called 'my_dataviz'
     svg = d3.select("#svgcontainer")
         .append("svg")
         .attr("width", svg_width)
         .attr("height", svg_height)
-        // .append("g")
         .attr('id', 'barsvg')
 
     console.log('appended svg')
@@ -70,7 +180,7 @@ function build_hop(predictions) {
 
     // domains
     xScale.domain(Array(num_data_points).fill().map((element, index) => index + 1));
-    yScale.domain([0, 0.5]);
+    yScale.domain([0, 0.6]);
 
     // x scale
     g.append('g')
@@ -89,22 +199,27 @@ function build_hop(predictions) {
     // Bars
     for (let i = 0; i < num_data_points; i++) {
         svg.append('rect')
-            .attr('fill', 'skyblue')
+            .attr('fill', color_by_category(predictions[i]))
             .attr('stroke', 'black')
             .attr('transform', 'translate('+margin.left+', '+ margin.bottom+')')
             .attr('x', xScale(i+1))
             .attr('y', yScale(predictions[i]))
             .attr('height', graph_height - yScale(predictions[i]))
             .attr("width", xScale.bandwidth())
+            // TODO add text for chances?
+            // .attr('text', )
     }
-
-    
-
-    
-    
 }
 
-// TODO update the plot every second
+function color_by_category(prediction) {
+    if (prediction < 0.1) return "lightcyan"
+    if (prediction < 0.3) return "skyblue"
+    return "deepskyblue"
+}
+
+/**
+ * Updates the HOP every second
+ */
 function update_hop() {
     timer = setInterval(function() { 
         remove_svg()
@@ -113,15 +228,21 @@ function update_hop() {
 
     // stopTimer()
     
-    return 0
 }
 
+/**
+ * Stops the timer and thus stops the HOPs from animating
+ */
 function stopTimer() {
     alert("Timer stopped");
     clearInterval(timer); 
 }
 
-// generate hypothetical data based on the predictions and chances
+
+/**
+ * Generates hypothetical data based on the predictions and chances
+ * @returns An array of hypothetical predictions
+ */
 function gen_hypothetical_data() {
     let hypothetical_predictions = []
     let categories = []
@@ -170,7 +291,10 @@ function gen_hypothetical_data() {
 }
 
 
-// Pick an outcome based on weighted categories/specs
+/**
+ * Picks an outcome based on weighted categories/specs
+ * @param {*} spec The weighted categories
+ */
 function weightedRand(spec) {
     var i, sum=0, r=Math.random();
 
@@ -181,9 +305,12 @@ function weightedRand(spec) {
 }
   
  
-//  build_hop(gen_rain_data(num_data_points))
-let chances = [0.84, 0.66, 0.61],
-    predictions = [0.08, 0.07, 0.06];
+/**------------------------------------------------------------------------
+ *                                RUN
+ *------------------------------------------------------------------------**/
+let foo_data = generate_hourly_data(),
+    predictions = foo_data[0],
+    chances = foo_data[1];
 
 og_data = predictions
 og_chances = chances
